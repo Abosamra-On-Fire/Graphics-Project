84af56478f74b818981662ba361e92714fc6c49b
Abosamra-On-Fire <kareem_k81@live.com>
2025-05-05 15:24:59 +0300

    :sparkles: second room monster dance

--- source/common/systems/movement.hpp
+++ source/common/systems/movement.hpp
@@ -16,6 +16,8 @@ namespace our
     // For more information, see "common/components/movement.hpp"
     class MovementSystem {
     public:
+        float time = 0;
+
 
         // This should be called every frame to update all entities containing a MovementComponent. 
         void update(World* world, float deltaTime) {
@@ -25,11 +27,14 @@ namespace our
                 MovementComponent* movement = entity->getComponent<MovementComponent>();
                 // If the movement component exists
                 if(movement){
-                    if(entity->name=="monster") {
-                    }
+                    time += deltaTime;
+                    if(entity->name=="monster2") {
+                        entity->localTransform.position += std::sin(time*3 ) * movement->linearVelocity;
+                    }else{
                     // Change the position and rotation based on the linear & angular velocity and delta time.
                     entity->localTransform.position += deltaTime * movement->linearVelocity;
                     entity->localTransform.rotation += deltaTime * movement->angularVelocity;
+                    }
                 }
             }
         }
